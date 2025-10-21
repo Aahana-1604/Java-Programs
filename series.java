a3eb3b595912f30631d7a8457511e83f341f7c86
Aahana-1604 <lakhanpalaahana248@gmail.com>
2025-10-21 16:45:15 +0530

    Create series.java

---
+++ series.java
@@ -0,0 +1,10 @@
+public class series {
+    public static void main(String[] args) {
+        int c = 1;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(c + " ");
+            c = c * 10 + 1;
+        }
+    }
+}
+//1, 11, 111, 1111,.......
